90a8f2a84ef1ed52fed48f0bd00855e54a7792d2
hinataizayoi <hinataizayoi@gmail.com>
2020-02-01 17:15:20 -0600

    homepage

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import TaskList from './TaskList';
+import IssueList from './IssueList';
 import NotificationList from './NotificationList';
+import {formatTime} from '../utils';
+import {INFO, DANGER} from './utils';
 
 export default class Profile extends React.Component {
   constructor(props) {
@@ -49,6 +52,45 @@ export default class Profile extends React.Component {
         deadline: new Date(),
       },
     ];
+    const currentIssues = [
+      {
+        title: 'Light potentially broke in Area H8',
+        location: 'Area H8',
+        created: new Date(),
+      },
+    ];
+    const unreadNotifications = [
+      {
+        title: 'Temperature rises 5 degrees in Area H3',
+        location: 'Area H3',
+        created: new Date(),
+      },
+      {
+        title: 'Temperature rises 6 degrees in Area H6',
+        location: 'Area H6',
+        created: new Date(),
+      },
+      {
+        title: 'Temperature rises 8 degrees in Area H7',
+        location: 'Area H7',
+        created: new Date(),
+      },
+      {
+        title: 'CO2 level is too low in Area H8',
+        location: 'Area H8',
+        created: new Date(),
+      },
+      {
+        title: 'Feed is potentially blocked in Feeder W3',
+        location: 'Feeder W3',
+        created: new Date(),
+      },
+      {
+        title: 'Good morning',
+        location: 'Control Center',
+        created: new Date(),
+      },
+    ];
     const {firstName, lastName, level, pp, location, position, workYear, taskNumber, workingHours} = this.state;
 
     return <div class="Pt(40px) W(80%) Mx(30px) Miw(700px) C(white)">
@@ -87,6 +129,16 @@ export default class Profile extends React.Component {
           pp={task.pp}/>,
         )}
 
+        <div class="Mt(30px) Py(8px) Px(20px) W(100%) Bgc($green-1) Fz(20px) C($green-3) "><i class="far fa-clone"></i> Unread notifications</div>
+
+        <div class="Mt(10px)">
+          {unreadNotifications.map((task) => <NotificationList key={task} title={task.title} location={task.location} created={formatTime(task.created)} type={INFO} status="unread"/>)}
+        </div>
+        <div class="Mt(30px) Py(8px) Px(20px) W(100%) Bgc($red-1) Fz(20px) C($red-3) "><i class="far fa-clone"></i> Current issues</div>
+
+        <div class="Mt(10px)">
+          {currentIssues.map((task) => <IssueList key={task} title={task.title} location={task.location} created={task.created} type="current"/>)}
+        </div>
 
       </div>
     </div>;

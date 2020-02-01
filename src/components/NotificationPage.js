cf9c4a0be362e1cd150c77779ab10cd7fb285010
hinataizayoi <hinataizayoi@gmail.com>
2020-02-01 16:56:32 -0600

    notification

--- src/components/NotificationPage.js
+++ src/components/NotificationPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NotificationList from './NotificationList';
 import {INFO, DANGER} from './utils';
+import {formatTime} from '../utils';
 
 export default class NotificationPage extends React.Component {
   constructor(props) {
@@ -87,12 +88,12 @@ export default class NotificationPage extends React.Component {
     return <div class="Mx(30px) Miw(500px)">
       <div class="Mt(30px) Py(8px) Px(20px) W(100%) Bgc($green-1) Fz(20px) C($green-3) "><i class="far fa-clone"></i> Unread notifications</div>
       <div class="Mt(10px)">
-        {unreadNotifications.map((task) => <NotificationList key={task} type={INFO} status="unread"/>)}
+        {unreadNotifications.map((task) => <NotificationList key={task} title={task.title} created={formatTime(task.created)} type={INFO} status="unread"/>)}
       </div>
 
       <div class="Mt(30px) Py(8px) Px(20px) W(100%) Bgc($blue-1) Fz(20px) C($blue-3)"><i class="far fa-clone"></i> Read notification</div>
       <div class="Mt(10px)">
-        {readNotifications.map((task) => <NotificationList key={task} type={DANGER} status="read"/>)}
+        {readNotifications.map((task) => <NotificationList key={task} title={task.title} created={formatTime(task.created)} type={INFO} status="read"/>)}
       </div>
     </div>;
   }
